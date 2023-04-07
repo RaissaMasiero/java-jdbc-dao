82e967eafaccbebf486961e775249b466d7425a9
Raissa <raissamasi2@gmail.com>
2023-04-07 14:08:20 -0300

    Classe Department

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Department d1 = new Department(1, "Livros");
+        System.out.println(d1);
+    }
+}
