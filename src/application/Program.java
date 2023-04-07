1214ba2d6dfdc4255bb198d3641cba5e34983006
Raissa <raissamasi2@gmail.com>
2023-04-07 14:37:33 -0300

    SellerDaoJDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,9 @@ public class Program {
 
         Department d1 = new Department(1, "Livros");
         Seller s1 = new Seller(2, "Bob", "bob@gmail.com", new Date(), 3000.0, d1);
+
+        SellerDAO sellerDAO = DaoFactory.createSellerDao();
+
         System.out.println(s1);
     }
 }
