0c81ec5cae78866661ee9e91e0d343e9a6217671
Raissa <raissamasi2@gmail.com>
2023-04-07 14:22:06 -0300

    classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department d1 = new Department(1, "Livros");
-        System.out.println(d1);
+        Seller s1 = new Seller(2, "Bob", "bob@gmail.com", new Date(), 3000.0, d1);
+        System.out.println(s1);
     }
 }
